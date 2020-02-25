18289ad76e850b2bad103cd74230faaab432ac66
MarggythG <johanna98g@gmail.com>
2020-02-24 20:12:14 -0500

    actualizacion de css en responsive, introduccion a js

---
+++ v1.3_CM_HV_R/v1.2_CM_HV_R/assets/js/main.js
@@ -0,0 +1,10 @@
+document.querySelector('html').onclick = function() {
+	alert('Ouch! Deja de pincharme');
+}
+
+window.onload = function(){
+
+
+var miImage = document.querySelector('html body aside');
+alert(miImage);
+}
